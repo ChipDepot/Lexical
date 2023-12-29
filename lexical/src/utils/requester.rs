695e16128432a5f044b8d2e7faf22c60bd9b6575
dnl <danieldavid2001@gmail.com>
2023-12-29 18:19:10 -0500

    Minor bug regarding the path of the Url fixed

--- lexical/src/utils/requester.rs
+++ lexical/src/utils/requester.rs
@@ -8,12 +8,12 @@ pub(crate) async fn send_context(app: &Application) -> Result<(), ()> {
     let app_name = app.name.clone();
     let url = file_handler::get_argument::<Url>("-b")
         .unwrap()
-        .join(&app_name)
+        .join(&format!("apps/{app_name}"))
         .unwrap();
 
     let client = Client::new();
 
-    match client.post(url.clone()).json(app).send().await {
+    match client.post(url.to_owned()).json(app).send().await {
         Ok(k) => match k.status() {
             StatusCode::OK => (),
             _ => panic!("Recived {} from {}", k.status(), url.to_string()),
